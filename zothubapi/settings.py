7324d5032c3857e65ba5deac2ec2d748bba11c44
nathanngabc <nkng1@uci.edu>
2023-05-30 12:08:33 -0700

    Update settings.py

--- zothubapi/settings.py
+++ zothubapi/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#hgaa-)3bd0m2emq5g#3_8z*la0@=dbf0++$22%%)qcs7uldog'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
